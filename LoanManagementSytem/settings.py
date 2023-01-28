3ffe899abe93cd5384d1c1180271208317165c43
Erotu Felix <erotufelixpascal2@gmail.com>
2023-01-28 10:10:58 +0300

    adding apps to settings.py

--- LoanManagementSytem/settings.py
+++ LoanManagementSytem/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'LoginApp',
+    'LoanApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
