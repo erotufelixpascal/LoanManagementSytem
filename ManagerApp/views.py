333f80648383c40ae95820ac69fe94b170612cf8
Erotu Felix <erotufelixpascal2@gmail.com>
2023-01-31 04:25:28 +0300

    initial

--- ManagerApp/views.py
+++ ManagerApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from ManagerApp import AdminLoginForm
+from .forms import AdminLoginForm
 from django.shortcuts import redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -15,5 +15,20 @@ from datetime import date
 
 from django.db.models import Sum
 # Create your views here.
+
+def super_login_view(request):
+    form = AdminLoginForm()
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('home'))
+    else:
+        if request.method =='POST':
+            form = AdminLoginForm(data = request.POST)
+        
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+
+                user = authenticate(request, username= username, password =password)
+
 
 
