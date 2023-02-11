060420ff2f3e1119f4afb001706dc3cd6e624e43
Erotu Felix <erotufelixpascal2@gmail.com>
2023-02-11 04:37:23 +0300

    initial

---
+++ LoanApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from LoanApp import views
+
+app_name = 'LoanApp'
+
+urlpatterns =[
+    path('loan-request/', views.LoanRequest, name='loan_request'),
+    path('loan-repayment/', views.LoanPayment, name='loan_payment'),
+    path('user-transaction/', views.UserTransaction, name= 'user_transaction'),
+    path('user-loan-history/', views.UserLoanHistory, name='user_loan_history'),
+    path('user-dashboard/',views.UserDashBoard, name='user_dashboard'),
+    
+
+]
