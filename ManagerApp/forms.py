dd28a40c9946ccbaa989c4207a2be7386b86601e
Erotu Felix <erotufelixpascal2@gmail.com>
2023-02-11 03:32:27 +0300

    initial

--- ManagerApp/forms.py
+++ ManagerApp/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
-from LoanApp import loanCategory
+from LoanApp.models import loanCategory
 
 class LoanCategoryForm(forms.ModelForm):
     class Meta:
