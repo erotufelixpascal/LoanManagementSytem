fd2909e11ab57f70fdc846b295dc66445b5db2d6
Erotu Felix <erotufelixpascal2@gmail.com>
2023-02-11 04:17:58 +0300

    initial

--- LoanApp/admin.py
+++ LoanApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import loanRequest, CustomerLoan, loanTransaction
+from .models import loanRequest, CustomerLoan, loanTransaction, loanCategory
 
 # Register your models here.
 admin.site.register(loanRequest)
