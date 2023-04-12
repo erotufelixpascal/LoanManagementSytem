3317673c57e274ddb7bb3b010ee5729e1aaceefe
Erotu Felix <erotufelixpascal2@gmail.com>
2023-04-12 12:34:29 +0300

    initial

--- LoanManagementSytem/urls.py
+++ LoanManagementSytem/urls.py
@@ -15,11 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from LoanApp import views
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('account/', include('LoginApp.urls')),
     path('manager/', include('ManagerApp.urls')),
     path('loan/', include('LoanApp.urls')),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+handler404 ='LoanApp.views.error_404_view'
+
