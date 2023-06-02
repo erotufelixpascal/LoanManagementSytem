1eab5db3e8cb6ee4b34c4cc3ddc4ca9df3c434ac
Felix Erotu <erotufelixpascal2@gmail.com>
2023-06-02 11:25:42 +0300

    bugfix2

--- LoanManagementSytem/settings.py
+++ LoanManagementSytem/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+#import dj_database_url
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR =os.path.join(BASE_DIR, 'templates')
+STATIC_DIR =os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'LoanManagementSytem.urls'
@@ -125,3 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [STATIC_DIR]
+
+# Media
+
+MEDIA_URL = '/media/'
+
+# login url
+
+LOGIN_URL = '/account/login/'
