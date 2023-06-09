0bdfaa8b7be9ff5b21f52433b493c81296e02b2f
Felix Erotu <erotufelixpascal2@gmail.com>
2023-06-09 11:46:04 +0300

    correctedpathforlogintemplate

--- LoginApp/views.py
+++ LoginApp/views.py
@@ -42,8 +42,9 @@ def sign_up_view(request):
 
                 else:
                     error ='Your password is not strong enough'
-
-    return render(request, 'LoginApp/signup.html', context={'form':form, 'user':"Customer Register", 'error': error})
+    
+    return render(request, 'LoginApp/templates/LoginApp/signup.html', context={'form':form, 'user':"Customer Register", 'error': error})
+    #return render(request, 'LoginApp/signup.html', context={'form':form, 'user':"Customer Register", 'error': error})
 
 def login_view(request):
     form= CustomerLoginForm()
@@ -62,9 +63,9 @@ def login_view(request):
                 return HttpResponseRedirect(reverse('home'))
 
         else:
-            return render(request, 'LoginApp/login.html', context={'form':form, 'user':"Customer Login", 'error':'Invalid Username or password'})
+            return render(request, 'LoginApp/templates/LoginApp/login.html', context={'form':form, 'user':"Customer Login", 'error':'Invalid Username or password'})
     
-    return render(request, 'LoginApp/login.html', context={'form':form, 'user':"Customer Login"})
+    return render(request, 'LoginApp/templates/LoginApp/login.html', context={'form':form, 'user':"Customer Login"})
 
 @login_required
 def logout_view(request):
@@ -83,7 +84,7 @@ def edit_customer(request):
             customer.save()
             return HttpResponseRedirect(reverse('home'))
         
-    return render(request, 'LoginApp/edit.html', context={'form':form})
+    return render(request, 'LoginApp/templates/LoginApp/edit.html', context={'form':form})
 
 
 
