3cd89d9e282e2490b4fc2e75578d0f85c594251f
Felix Erotu <erotufelixpascal2@gmail.com>
2023-10-12 12:06:22 +0300

    FixLoanApp

--- LoanApp/models.py
+++ LoanApp/models.py
@@ -6,7 +6,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey('LoginApp.CustomerSignUp', on_delete=models.CASCADE, related_name='customer_related')
+
+    def __str__(self):
+        return self.user
     
 
 class loanCategory(models.Model):
